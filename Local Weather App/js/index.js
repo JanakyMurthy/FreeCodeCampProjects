b635d5aac455ad082b597ecba078bb17b6449bbf
janaky murthy <janaky96@yahoo.com>
2017-02-16 22:32:08 +0530

    removed geolocation api
    changed to get latitude and longitude based on ip address

--- Local Weather App/js/index.js
+++ Local Weather App/js/index.js
@@ -1,56 +1,50 @@
-window.onload = getLocation();
+window.onload = displayWeather();
 
 // disable caching
 $(document).ready(function() {
   $.ajaxSetup({ cache: false });
 });
 
-// get the location and pass it to displayWeather
-function getLocation()
-{
-   if(navigator.geolocation) {
-     // The coordinate object of the  location is passed to the displayWeathe function
-     navigator.geolocation.getCurrentPosition(displayWeather);
-   }
-   else { // geolocation not supported
-     var msg = document.getElementById("content");
-     
-     msg.innerHTML = "Geolocation not supported in your browser!!";
-   }
-}
 function displayWeather(position) {
-
-  // Get the Latitude and Longitude
-  var latitude = position.coords.latitude;
-  var longitude = position.coords.longitude;
   
-  // get the unit for temperature
-  var unit = "";
-  if($("#unit").html() == "C") unit = "metric";
-  else unit = "imperial";
+  // get lat and long based on ip address
+   $.getJSON("http://ip-api.com/json/?callback=?",
+    //get location api
+    function(data) {
+        
+       var latitude = data.lat;
+       var longitude = data.lon;
+     
+      // get the unit for temperature
+      var unit = "";
+      if($("#unit").html() == "C") unit = "metric";
+      else unit = "imperial";
 
-  // url to get temp from openweathermap
-  var url = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&APPID=53ea5849e3f266f4bce806b7d7e5dbdb"+"&units="+unit;
+      // url to get temp from openweathermap
+      var url = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&APPID=53ea5849e3f266f4bce806b7d7e5dbdb"+"&units="+unit;
 
-  
-// get weather from OpenWeather Map
-$.getJSON(url,function(json){
 
-   // get the needed parameters
-    var country = json.sys.country;
-    var city = json.name;
-    var temp = json.main.temp;
-    var description = json.weather[0].description;
-    var iconUrl = "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png"
-    
-    // set the values of parameters in divs
-    $("#location").html(city + ", " + country);
-    $("#tempVal").html(temp);
-    $("#description").html(description);
-    $("#weather-icon").attr('src',iconUrl);
-  }).fail( function(json, textStatus, error) {
-        alert("getJSON failed, status: " + textStatus + ", error: "+error);
- });
+    // get weather from OpenWeather Map
+    $.getJSON(url,function(json){
+
+       // get the needed parameters
+        var country = json.sys.country;
+        var city = json.name;
+        var temp = json.main.temp;
+        var description = json.weather[0].description;
+        var iconUrl = "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png"
+
+        // set the values of parameters in divs
+        $("#location").html(city + ", " + country);
+        $("#tempVal").html(temp);
+        $("#description").html(description);
+        $("#weather-icon").attr('src',iconUrl);
+      }).fail( function(json, textStatus, error) {
+            alert("getJSON failed, status: " + textStatus + ", error: "+error);
+     });
+
+   });
+ 
 }
 
 $("#unit").click(function() {
@@ -63,5 +57,5 @@ $("#unit").click(function() {
   else $("#unit").html("C");
   
   // get temp again
-  getLocation();
+  displayWeather();
 });
